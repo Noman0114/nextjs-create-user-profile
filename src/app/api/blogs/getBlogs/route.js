37d32ceba32100a860ea23b9e7d74228e6605c10
Numan Ahmad <139751760+Noman0114@users.noreply.github.com>
2024-09-18 06:00:45 -0700

    Update route.js

--- src/app/api/blogs/getBlogs/route.js
+++ src/app/api/blogs/getBlogs/route.js
@@ -2,7 +2,7 @@ import { connect } from '@/dbConfig/dbConfig';
 import blogData from '@/models/blogModel';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic'
+
 export async function GET() {
     try {
 connect();
