24466329c346eb224ddb346b389047b7b7cb95b8
Numan Ahmad <139751760+Noman0114@users.noreply.github.com>
2024-09-19 01:01:53 -0700

    Update route.js

--- src/app/api/blogs/getBlogs/route.js
+++ src/app/api/blogs/getBlogs/route.js
@@ -2,17 +2,20 @@ import { connect } from '@/dbConfig/dbConfig';
 import blogData from '@/models/blogModel';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     try {
-connect();
+        // Wait for the database to connect
+        await connect();
+
         // Fetch all blog posts from the database
         const blogs = await blogData.find({});
         
         // Return the blogs as a JSON response
         return NextResponse.json(blogs);
     } catch (error) {
-        console.error('Error fetching blog data:', error);
+        console.error('Error fetching blog data:', error.message);
         return NextResponse.json({ error: 'Failed to fetch blogs' }, { status: 500 });
     }
 }
